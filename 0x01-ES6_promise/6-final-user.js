536ada8adf588643addb5eadaef38e0f78ac9eae
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-03-07 14:57:10 +0100

    return promise of an object array

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
+    .then((results) => (results.map((result) => ({ status: result.status, body: result.value }))));
+}
