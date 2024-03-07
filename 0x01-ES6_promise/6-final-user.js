852a95c3439c5e64409fb6b261d00f9c49be9ba3
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-03-07 15:14:53 +0100

    ok

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then((results) => (results.map((result) => ({ status: result.status, value: result.status === 'fulfilled' ? result.value : String(result.reason) }))));
+  return Promise
+    .allSettled([
+      uploadPhoto(fileName), signUpUser(firstName, lastName),
+    ])
+    .then((results) => (
+      results.map((result) => (
+        {
+          status: result.status,
+          value: result.status === 'fulfilled' ? result.value : String(result.reason),
+        }
+      ))
+    ));
 }
