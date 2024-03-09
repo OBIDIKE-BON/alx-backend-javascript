79e023708b55c89602cab5ea7ec224bf0085e157
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-03-09 05:51:00 +0100

    ok

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,8 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [new ClassRoom(20), new ClassRoom(15), new ClassRoom(10)];
+  const room1 = new ClassRoom(20);
+  const room2 = new ClassRoom(15);
+  const room3 = new ClassRoom(10);
+  return [room1, room2, room3];
 }
