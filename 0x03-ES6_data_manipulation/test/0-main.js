b2c820bc3af176551652193b93c1bbc02c46d6e3
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-03-12 16:50:46 +0100

    update maps

--- 0x03-ES6_data_manipulation/test/0-main.js
+++ 0x03-ES6_data_manipulation/test/0-main.js
@@ -4,7 +4,14 @@ import createInt8TypedArray from '../5-typed_arrays';
 import setFromArray from '../6-set';
 import hasValuesFromArray from '../7-has_array_values';
 import cleanSet from '../8-clean_set';
+import updateUniqueItems from '../10-update_uniq_items';
 import groceriesList from '../9-groceries_list';
+
+const map = groceriesList();
+console.log(map);
+
+updateUniqueItems(map);
+console.log(map);
 
 console.log(groceriesList());
 
