c46a9ee42127f2e00f1936682f31e1db75c3c55c
OBIDIKE-BON <bonifaceobidike@gmail.com>
2024-05-31 05:02:59 +0100

    ok

--- 0x02-ES6_classes/test/main.js
+++ 0x02-ES6_classes/test/main.js
@@ -1,4 +1,9 @@
 import ClassRoom from '../0-classroom';
+import initializeRooms from '../1-make_classrooms';
 
+// test 0
 const classroom = new ClassRoom(10);
 console.log(classroom._maxStudentsSize);
+
+// test 1
+console.log(initializeRooms());
